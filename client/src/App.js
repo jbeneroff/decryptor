0e4e828478dbecedf7c21390446a00576302f9b5
Jared Beneroff <jaredbeneroff@Jareds-MacBook-Pro.local>
2021-08-25 15:13:11 -0400

    react app and file structure

---
+++ client/src/App.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react'
+import { Switch, Route, useHistory } from 'react-router-dom'
+import MainContainer from './containers/MainContainer'
+import Layout from './layouts/Layout'
+import Login from './screens/Login'
+import SignUp from './screens/SignUp'
+import { loginUser, registerUser, verifyUser, removeToken } from './services/auth'
+import './App.css'
+
+function App() {
+  const [currentUser, setCurrentUser] = useState(null)
+  const history = useHistory()
+
+  useEffect(() => {
+    const handleVerify = async () => {
+      const userData = await verifyUser()
+      setCurrentUser(userData)
+    };
+    handleVerify()
+  }, [])
+
+  const handleLogin = async (loginData) => {
+    const userData = await loginUser(loginData)
+    setCurrentUser(userData)
+    history.push('/')
+  };
+
+  const handleRegister = async (registerData) => {
+    const userData = await registerUser(registerData)
+    setCurrentUser(userData)
+    history.push('/')
+  };
+
+  const handleLogout = () => {
+    setCurrentUser(null)
+    localStorage.removeItem('authToken')
+    removeToken()
+    history.push('/')
+  };
+
+  return (
+    <div className="App">
+      <Layout currentUser={currentUser} handleLogout={handleLogout}>
+        <Switch>
+          <Route path='/login'>
+            <Login handleLogin={handleLogin} />
+          </Route>
+          <Route path='/register'>
+            <SignUp handleRegister={handleRegister} />
+          </Route>
+          <Route path='/'>
+            <MainContainer currentUser={currentUser} />
+          </Route>
+        </Switch>
+      </Layout>
+    </div>
+  )
+}
+
+export default App
