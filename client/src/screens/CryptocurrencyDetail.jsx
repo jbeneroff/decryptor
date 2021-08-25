db9ff9a6b2627c054d9e810c4cf6452e03b7c426
Jared Beneroff <jaredbeneroff@Jareds-MacBook-Pro.local>
2021-08-25 16:31:55 -0400

    crypto details

--- client/src/screens/CryptocurrencyDetail.jsx
+++ client/src/screens/CryptocurrencyDetail.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { getOneCryptocurrency } from '../services/cryptocurrencies'
 
-export default function CryptocurrencyDetail() {
+export default function CryptocurrencyDetail(props) {
+
+  const [cryptocurrency, setCryptocurrency] = useState(null)
+  const { id } = useParams()
+  const { posts } = props
+
+  useEffect(() => {
+    const fetchCryptocurrency = async () => {
+      const cryptocurrencyData = await getOneCryptocurrency(id)
+      setCryptocurrency(cryptocurrencyData)
+      console.log(cryptocurrencyData)
+    }
+    fetchCryptocurrency()
+  }, [id])
+
   return (
     <div>
-      <h1>Crypto Details</h1>
+      <h1>{cryptocurrency?.name}</h1>
+      <h2>{cryptocurrency?.symbol}</h2>
+      <p>{cryptocurrency?.description}</p>
+      {cryptocurrency?.posts.map((post) => (
+        <div key={post.id}>
+          <p>{post.content}</p>
+          <p>{post.user_id.username}</p>
+        </div>
+      ))}
     </div>
   )
 }
