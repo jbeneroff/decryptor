40b91ffb6a75d26613d9e00b6035c8b642c1ad2c
Jared Beneroff <jaredbeneroff@Jareds-MacBook-Pro.local>
2021-08-30 12:24:44 -0400

    button hover

--- client/src/screens/CryptocurrencyDetail.jsx
+++ client/src/screens/CryptocurrencyDetail.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import CommentCreate from '../components/CommentCreate'
-import PostCreate from './PostCreate'
+// import PostCreate from './PostCreate'
 import { getOneCryptocurrency } from '../services/cryptocurrencies'
 import Loader from '../components/Loader'
 import './CryptocurrencyDetail.css'
@@ -9,9 +9,11 @@ import './CryptocurrencyDetail.css'
 export default function CryptocurrencyDetail(props) {
 
   const [cryptocurrency, setCryptocurrency] = useState(null)
+  // const [isCreatePostShow, setIsCreatePostShow] = useState(false)
+  const [isDescriptionShow, setIsDescriptionShow] = useState(false)
   const [isCommentsShow, setIsCommentsShow] = useState(false)
   const { id } = useParams()
-  const { cryptocurrencies, handleCreate, posts, handleDelete, currentUser, comments, handleCreateComment, handleDeleteComment } = props
+  const { cryptocurrencies, posts, handleDelete, currentUser, comments, handleCreateComment, handleDeleteComment } = props
 
   useEffect(() => {
     const fetchCryptocurrency = async () => {
@@ -29,10 +31,25 @@ export default function CryptocurrencyDetail(props) {
     }
   }
 
+  const showDescription = (cryptocurrency) => {
+    if (isDescriptionShow === cryptocurrency.id) {
+      setIsDescriptionShow(false)
+    } else {
+      setIsDescriptionShow(cryptocurrency.id)
+    }
+  }
+
+  // const showCreatePost = (post) => {
+  //   if (isCreatePostShow === post.id) {
+  //     setIsCreatePostShow(false)
+  //   } else {
+  //     setIsCreatePostShow(post.id)
+  //   }
+  // }
+
   if (posts.length === 0) {
     return <Loader />
   }
-
 
   return (
     <div className ='page'>
@@ -50,7 +67,9 @@ export default function CryptocurrencyDetail(props) {
       <div className='detail-div'>
         <h1 id='crypto-detail-name'>{cryptocurrency?.name}</h1>
         <h2 id='crypto-detail-symbol'>{cryptocurrency?.symbol}</h2>
-        <h3 id='crypto-detail-description'>{cryptocurrency?.description}</h3>
+        <button id='show-description' onClick={() => showDescription(cryptocurrency)}>What is {cryptocurrency?.name}?</button>
+        {isDescriptionShow === cryptocurrency?.id &&
+          <h3 id='crypto-detail-description'>{cryptocurrency?.description}</h3>}
       </div>
       {currentUser && (
         <div>
@@ -77,7 +96,8 @@ export default function CryptocurrencyDetail(props) {
                   </div>
                 )}
                 <button id='show-button' onClick={() => showComments(post)}>Comments</button>
-                {isCommentsShow === post.id && <div>
+                {isCommentsShow === post.id && 
+                <div>
                   {comments.map((comment, key) => {
                     if (comment?.post_id === post?.id) {
                       return (
